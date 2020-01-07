5266a7866c4b4fe42d56343590299111d97d4af8
Leo Sun <leosunsl@outlook.com>
2020-01-07 14:09:02 +0800

    use a dynamic password(md5 hash) as aes key input

--- crypto/crypto_test.py
+++ crypto/crypto_test.py
@@ -2,17 +2,21 @@
 import os
 
 from Crypto.Cipher import AES
+from Crypto.Hash import MD5
 from Crypto.Util.Padding import pad
 from Crypto.Util.Padding import unpad
 from Crypto.Random import get_random_bytes
 
 if __name__ == '__main__':
+    # readable password to hash
+    password = input("Please input a password: ")
+    data = input("Please input your message: ")
 
     # encrypt
-    data = b'secret message?'
-    key = get_random_bytes(16)
+    hasher = MD5.new(password.encode('utf-8'))
+    key = hasher.digest()
     cipher = AES.new(key, AES.MODE_CBC)
-    ct_bytes = cipher.encrypt(pad(data, AES.block_size))
+    ct_bytes = cipher.encrypt(pad(data.encode('utf-8'), AES.block_size))
     iv = cipher.iv
 
     # decrypt
