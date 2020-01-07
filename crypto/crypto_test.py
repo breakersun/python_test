153ef689f681ea73edcab905588ae688d2089db5
Leo Sun <leosunsl@outlook.com>
2020-01-07 14:27:04 +0800

    use a json to transfer crypted message(iv and ciphtertext)
    tx/rx share: key

--- crypto/crypto_test.py
+++ crypto/crypto_test.py
@@ -1,6 +1,7 @@
 
 import os
-
+import base64
+import json
 from Crypto.Cipher import AES
 from Crypto.Hash import MD5
 from Crypto.Util.Padding import pad
@@ -17,9 +18,16 @@ if __name__ == '__main__':
     key = hasher.digest()
     cipher = AES.new(key, AES.MODE_CBC)
     ct_bytes = cipher.encrypt(pad(data.encode('utf-8'), AES.block_size))
-    iv = cipher.iv
+    ct = base64.b64encode(ct_bytes).decode('utf-8')
+    iv = base64.b64encode(cipher.iv).decode('utf-8')
+    result = json.dumps({'iv':iv, 'ciphertext':ct})
+    print(result)
+
 
     # decrypt
+    b64 = json.loads(result)
+    iv = base64.b64decode(b64['iv'])
+    ct_bytes = base64.b64decode(b64['ciphertext'])
     decryptor = AES.new(key, AES.MODE_CBC, iv)
     dt_bytes = unpad(decryptor.decrypt(ct_bytes), AES.block_size)
     print(dt_bytes)
