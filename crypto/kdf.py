fe73179367a97c59f7c2449a50bc43e000260495
Leo Sun <leosunsl@outlook.com>
2020-07-27 15:29:10 +0800

    time measurements of different iterations of pbkdf2

--- crypto/kdf.py
+++ crypto/kdf.py
@@ -1,8 +1,11 @@
 from backports.pbkdf2 import pbkdf2_hmac
-import binascii
+import binascii, time
 
 salt = binascii.unhexlify('aaef2d3f4d77ac66e9c5a6c3d8f921d1')
 password = 'p@$Sw0rD~1'.encode('utf-8')
+start_time = time.time()
 key = pbkdf2_hmac('sha256', password, salt, 50000, 32)
+end_time = time.time()
 
 print('Derived key:', binascii.hexlify(key))
+print('Excution time: %s Seconds', end_time - start_time)
