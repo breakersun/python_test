7c3d3413c86bed9e6a34adc86b839d339ef080fd
Leo Sun <leosunsl@outlook.com>
2020-07-28 09:09:19 +0800

    add entropy collection into pseudo-random-generator

--- crypto/random_test.py
+++ crypto/random_test.py
@@ -1,6 +1,7 @@
 import random, time
 import secrets
 import hashlib
+import binascii
 
 random.seed(time.time())
 r1 = random.randrange(1e49, 1e50-1)
@@ -11,8 +12,17 @@ r2 = random.randrange(1e49, 1e50-1)
 # print(r2)
 # print(secrets.randbelow(int(1e50)))
 
-startSeed = str(time.time()) + '|'
-print(startSeed)
+entropy = ''
+for i in range(5):
+    s = input("Enter Something[" + str(i + 1) + 'of 5]:')
+    entropy = entropy + s + '|' + str(time.time()) + '|'
+
+print("Entropy:", entropy)
+startSeed = str(binascii.hexlify(hashlib.sha256(entropy.encode('ascii')).digest()))[2:-1]
+print('Start seed = SHA-256(entopy) = ', startSeed)
+
+# startSeed = str(time.time()) + '|'
+# print(startSeed)
 min = 10
 max = 20
 for i in range(5):
