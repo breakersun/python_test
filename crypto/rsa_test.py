e5c93f99bc40a6be9d57806fd1230363b8a42baf
Leo Sun <leosunsl@outlook.com>
2020-07-29 15:52:57 +0800

    simple RSA demo

--- crypto/rsa_test.py
+++ crypto/rsa_test.py
@@ -3,20 +3,9 @@ from Crypto.Cipher import PKCS1_OAEP
 
 import binascii
 
-keyPair = RSA.generate(1024)
+key = RSA.importKey(open('public.pem').read())
 
-pubKey = keyPair.publickey()
-pubKeyPEM = pubKey.export_key()
-print(pubKeyPEM.decode('ascii'))
-
-privatePEM = keyPair.export_key()
-print(privatePEM.decode('ascii'))
-
-plaintext = b'a plain text message'
-encryptor = PKCS1_OAEP.new(pubKey)
-ciphermsg = encryptor.encrypt(plaintext)
-print('Encrypted : ', binascii.hexlify(ciphermsg))
-
-decrytor = PKCS1_OAEP.new(keyPair)
-decryptedMsg = decrytor.decrypt(ciphermsg)
-print('Decrypted : ', decryptedMsg)
+encryptor = PKCS1_OAEP.new(key)
+inputMsg = input('Input Msg : ')
+encryptedMsg = encryptor.encrypt(inputMsg.encode('ascii'))
+print("Encrypted : ", binascii.hexlify(encryptedMsg))
