e40beeb0cc3099d2a0cc2ed5280d96a79de356e6
Leo Sun <leosunsl@outlook.com>
2020-07-31 10:55:35 +0800

    rsa signature/verify evolved with "PKCS#1 RSA digital signatures"

--- crypto/rsa_sign.py
+++ crypto/rsa_sign.py
@@ -1,13 +1,31 @@
+import binascii
 from Crypto.PublicKey import RSA
+from Crypto.Signature.pkcs1_15 import PKCS115_SigScheme
+from Crypto.Hash import SHA256
 
 keypair = RSA.generate(1024)
-print(f'pub key : (n = {hex(keypair.n)}, e = {hex(keypair.e)})')
-print(f'private key : (n = {hex(keypair.n)}, d = {hex(keypair.d)})')
+pubkey = keypair.publickey()
 
 msg = b'message for signing'
-from hashlib import sha512
-hash = int.from_bytes(sha512(msg).digest(), byteorder='big')
-sign = pow(hash, keypair.d, keypair.n)
-hash_from_sign = pow(sign, keypair.e, keypair.n)
-print(sign)
-print('Sign valid ', hash == hash_from_sign)
+hash = SHA256.new(msg)
+signer = PKCS115_SigScheme(keypair)
+sign = signer.sign(hash)
+print('Signature:', binascii.hexlify(sign))
+
+msg = b'message for signing'
+hash = SHA256.new(msg)
+verifier = PKCS115_SigScheme(pubkey)
+try:
+    verifier.verify(hash, sign)
+    print('Signature is valid')
+except:
+    print('Signa')
+
+msg = b'tampered message for signing'
+hash = SHA256.new(msg)
+verifier = PKCS115_SigScheme(pubkey)
+try:
+    verifier.verify(hash, sign)
+    print('Signature is valid')
+except:
+    print('Signature is invalid')
